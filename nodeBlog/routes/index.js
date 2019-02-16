b7e54e90a45c074484fddc2b2766d232c3869b80
财务前端_李萌 <limroid0408@163.com>
2019-02-16 20:39:34 +0800

    feat： 完整版提交

--- nodeBlog/routes/index.js
+++ nodeBlog/routes/index.js
@@ -6,15 +6,25 @@ var formatDate = require('./../public/javascripts/untils')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const query = 'SELECT * FROM article ORDER BY articleID DESC'
-  mysql.query(query, (err, rows, fields) => {
+  const user = req.session.user
+  const page = req.query.page || 1
+  const start = (page - 1) * 8
+  const end = page * 8
+  const queryCount = 'SELECT COUNT(*) AS articleNum FROM article'
+  const queryArticle = 'SELECT * FROM article ORDER BY articleID DESC LIMIT ' + start + ',' + end
+  mysql.query(queryArticle, (err, rows, fields) => {
     let articles = rows
     if (Array.isArray(articles)) {
-      rows.map(item => {
+      articles.map(item => {
         item.articleTime = formatDate(item.articleTime)
       })
     }
-    res.render('index', { articles, user: req.session.user })
+    mysql.query(queryCount, (err, rows, fields) => {
+      const articleNum = rows[0].articleNum
+      const pageNum = Math.ceil(articleNum / 8)
+      console.log(pageNum, page)
+      res.render('index', { articles, user, pageNum, page })
+    })
   })
 })
 
@@ -29,6 +39,47 @@ router.get('/logout', function(req, res, next) {
   res.redirect('/')
 })
 
+/* 文章内容页 */
+router.get('/articles/:articleID', function(req, res, next) {
+  const articleID = req.params.articleID
+  const query = 'SELECT * FROM article WHERE articleID=' + mysql.escape(articleID)
+  mysql.query(query, (err, rows, fields) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const queryForClick =
+      'UPDATE article SET articleClick=articleClick + 1 WHERE articleID=' + mysql.escape(articleID)
+    let article = rows[0]
+    mysql.query(queryForClick, (err, rows, fields) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      article.articleTime = formatDate(article.articleTime)
+      res.render('article', { article, user: req.session.user })
+    })
+  })
+})
+
+/* 文章新增页 */
+router.get('/add', function(req, res, next) {
+  const user = req.session.user
+  if (!user) {
+    return res.redirect('/login')
+  }
+  res.render('add', { user: req.session.user })
+})
+
+/* 友情链接页面 */
+router.get('/friends', function(req, res, next) {
+  res.render('friends', { user: req.session.user })
+})
+
+/* 关于博客页面 */
+router.get('/about', function(req, res, next) {
+  res.render('about', { user: req.session.user })
+})
 /* 登录信息验证 */
 router.post('/login', function(req, res, next) {
   var name = req.body.name
@@ -55,40 +106,8 @@ router.post('/login', function(req, res, next) {
   })
 })
 
-/* 文章内容页 */
-router.get('/articles/:articleID', function(req, res, next) {
-  const articleID = req.params.articleID
-  const query = 'SELECT * FROM article WHERE articleID=' + mysql.escape(articleID)
-  mysql.query(query, (err, rows, fields) => {
-    if (err) {
-      console.log(err)
-      return
-    }
-    const queryForClick =
-      'UPDATE article SET articleClick=articleClick + 1 WHERE articleID=' + mysql.escape(articleID)
-    let article = rows[0]
-    mysql.query(queryForClick, (err, rows, fields) => {
-      if (err) {
-        console.log(err)
-        return
-      }
-      article.articleTime = formatDate(article.articleTime)
-      res.render('article', { article, user: req.session.user })
-    })
-  })
-})
-
-/* 写文章界面页 */
-router.get('/edit', function(req, res, next) {
-  const user = req.session.user
-  if (!user) {
-    return res.redirect('/login')
-  }
-  res.render('edit', { user: req.session.user })
-})
-
 /* 新增文章 */
-router.post('/edit', function(req, res, next) {
+router.post('/add', function(req, res, next) {
   var title = req.body.title
   var content = req.body.content
   var author = req.session.user.authorName
@@ -105,13 +124,54 @@ router.post('/edit', function(req, res, next) {
   })
 })
 
-/* 友情链接页面 */
-router.get('/friends', function(req, res, next) {
-  res.render('friends', { user: req.session.user })
+/* 修改文章界面 */
+router.get('/modify/:articleID', function(req, res, next) {
+  var articleID = req.params.articleID
+  var user = req.session.user
+  const query = 'SELECT * FROM article WHERE articleID=' + mysql.escape(articleID)
+  if (!user) {
+    return res.redirect('/login')
+  }
+  mysql.query(query, (err, rows, fields) => {
+    if (err) {
+      return console.log(err)
+    }
+    const article = rows[0]
+    const title = article.articleTitle
+    const content = article.articleContent
+    console.log(title, content)
+    res.render('modify', { user, title, content, articleID })
+  })
 })
 
-/* 关于博客页面 */
-router.get('/about', function(req, res, next) {
-  res.render('about', { user: req.session.user })
+/* 修改文章 */
+router.post('/modify/:articleID', function(req, res, next) {
+  var articleID = req.params.articleID
+  // var user = req.session.user
+  var title = req.body.title
+  var content = req.body.content
+  var query = `UPDATE article SET articleTitle = ${mysql.escape(
+    title
+  )}, articleContent = ${mysql.escape(content)} WHERE articleID= ${mysql.escape(articleID)}`
+  console.log(query)
+  mysql.query(query, (err, rows, fields) => {
+    if (err) {
+      return console.log(err)
+    }
+    res.redirect('/')
+  })
+})
+
+/* 删除文章 */
+router.get('/delete/:articleID', function(req, res, next) {
+  var articleID = req.params.articleID
+  var user = req.session.user
+  var query = `DELETE FROM article WHERE articleID= ${mysql.escape(articleID)}`
+  if (!user) {
+    return res.redirect('/login')
+  }
+  mysql.query(query, (err, rows, fields) => {
+    res.redirect('/')
+  })
 })
 module.exports = router
